874cb5bff39bc92f66238c275eef06e40eca5404
the-darkesttt <belladonna.om@gmail.com>
2026-05-06 02:12:00 +0300

    save typed guess function

--- app/js/script.js
+++ app/js/script.js
@@ -1,9 +1,18 @@
 const lettersPattern = /^[A-Za-z][A-Za-z0-9]*$/;
+let currentGuessCount = 1;
+let currentGuess = document.querySelector("#guess" + currentGuessCount);
 
 document.addEventListener("keydown", (e) => {
     let keyPress = e.key;
     console.log(e.key);
-    if (keyPress.length == 1) {
-        console.log(lettersPattern.test(e.key));
+    if (keyPress.length == 1 && lettersPattern.test(e.key)) {
+        // console.log("is letter");
+        updateLetters(keyPress);
     }
 });
+
+const updateLetters = (letter) => {
+    currentGuess.dataset.letters = currentGuess.dataset.letters + letter;
+};
+
+const updateTiles = () => {};
