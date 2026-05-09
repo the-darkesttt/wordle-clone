04a09520075f93fc5bb5156e9f397111759fb4fe
the-darkesttt <belladonna.om@gmail.com>
2026-05-09 23:21:25 +0300

    choose random solution word via rindomizing an array

--- app/js/script.js
+++ app/js/script.js
@@ -2,12 +2,26 @@ const lettersPattern = /^[A-Za-z][A-Za-z0-9]*$/;
 let currentGuessCount = 1;
 let currentGuess = document.querySelector("#guess" + currentGuessCount);
 
+let words = ["baker", "store", "horse", "clone", "speak", "apple"];
+let solutionWord = "";
+const chooseWord = () => {
+    let randomItem = Math.floor(Math.random() * (words.length - 1)) + 1;
+    solutionWord = words[randomItem];
+};
+
+chooseWord();
+console.log(solutionWord);
+
 document.addEventListener("keydown", (e) => {
     let keyPress = e.key;
     console.log(e.key);
-    if (keyPress.length == 1 && lettersPattern.test(e.key)) {
+    if (
+        keyPress.length == 1 &&
+        lettersPattern.test(e.key) &&
+        currentGuess.dataset.letters.length < 5
+    ) {
         updateLetters(keyPress);
-    } else if (e.key == "Backspace") {
+    } else if (e.key == "Backspace" && currentGuess.dataset.letters != "") {
         deleteFromLetters();
     }
 });
@@ -29,6 +43,7 @@ const updateTiles = (tileNumber, letter) => {
 const deleteFromLetters = () => {
     let oldLetters = currentGuess.dataset.letters;
     let newLetters = oldLetters.slice(0, -1);
+    currentGuess.dataset.letters = newLetters;
     deleteFromTiles(oldLetters.length);
 };
 
